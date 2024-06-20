ac3a24ed5223b71857d66068b1fb4e82be6cc458
ROZZNN <av5gust.slap@yandex.ru>
2024-06-21 00:30:22 +0300

    config update

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -1,6 +1,5 @@
 import globals from "globals";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
 import path from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -9,11 +8,31 @@ import pluginJs from "@eslint/js";
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended });
 
 export default [
-  {languageOptions: { globals: globals.browser }, 
-  plugins: {pluginReactConfig}},
-  ...compat.extends("airbnb"),
-  // pluginReactConfig,
+    {
+        languageOptions: { globals: globals.browser },
+        plugins: { react: pluginReactConfig },
+        rules: {
+            "import/extensions": [
+                "error",
+                "ignorePackages",
+                {
+                    "js": "always",
+                    "jsx": "always"
+                }
+            ],
+            "no-undef": "error",
+            "import/no-named-as-default": "error",
+            "import/no-named-as-default-member": "error",
+            "react/react-in-jsx-scope": "off",
+            "react/jsx-filename-extension": [
+                "error",
+                { "extensions": [".jsx", ".js"] }
+            ],
+            "func-names": ["error", "always"]
+        },
+        ...compat.extends("airbnb")
+    }
 ];
