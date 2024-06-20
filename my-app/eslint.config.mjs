ff333613f2f0f2978c5505147f2fa4417f1dab3e
ROZZNN <av5gust.slap@yandex.ru>
2024-06-21 00:51:12 +0300

    ацпцкупукпкцу

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -1,5 +1,6 @@
 import globals from "globals";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
 import path from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -11,28 +12,7 @@ const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended });
 
 export default [
-    {
-        languageOptions: { globals: globals.browser },
-        plugins: { react: pluginReactConfig },
-        rules: {
-            "import/extensions": [
-                "error",
-                "ignorePackages",
-                {
-                    "js": "always",
-                    "jsx": "always"
-                }
-            ],
-            "no-undef": "error",
-            "import/no-named-as-default": "error",
-            "import/no-named-as-default-member": "error",
-            "react/react-in-jsx-scope": "off",
-            "react/jsx-filename-extension": [
-                "error",
-                { "extensions": [".jsx", ".js"] }
-            ],
-            "func-names": ["error", "always"]
-        },
-        ...compat.extends("airbnb")
-    }
+    { languageOptions: { globals: globals.browser } },
+    ...compat.extends("airbnb"),
+    pluginReactConfig,
 ];
